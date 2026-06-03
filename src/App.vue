81870fb33f912329c2fdd8827d8f4fe85b0518d7
Maximilian Rukavina <maximilian.rukavina@gmail.com>
2026-06-03 21:14:59 +0200

    font setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-black text-white p-4">
+  <div>
     <h1 class="text-3xl">You did it!</h1>
     <p>
       Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
