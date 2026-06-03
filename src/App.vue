9401a5b5a2f9ae1ea8f1189e10520d8a1183c754
Maximilian Rukavina <maximilian.rukavina@gmail.com>
2026-06-03 20:39:10 +0200

    Initial Vue Setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-black text-white p-4">
+    <h1 class="text-3xl">You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
