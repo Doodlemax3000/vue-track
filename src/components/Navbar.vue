769c40abb666fa8bf165cd44bea6fe20702c2b0d
Maximilian Rukavina <maximilian.rukavina@gmail.com>
2026-06-07 14:13:39 +0200

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import Button from '@/components/ui/Button.vue'
+import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
+const router = useRouter()
+
+async function logout() {
+  try {
+    await authStore.logout()
+    router.push('/')
+  } catch (error) {
+    console.error('Logout error:', error)
+  }
+}
 </script>
 
 <template>
@@ -12,7 +23,7 @@ const authStore = useAuthStore()
     </RouterLink>
     <div v-if="authStore.user" class="ml-auto flex items-center gap-4">
       <p>{{ authStore.user.email }}</p>
-      <Button class="btn-neutral">Logout</Button>
+      <Button class="btn-neutral" @click="logout">Logout</Button>
     </div>
     <div v-else class="ml-auto flex items-center gap-4">
       <RouterLink to="/login">
